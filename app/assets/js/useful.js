173d3a5bd7ea5b6519db40ff3d9e4f06d20eeb4e
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-12-27 21:41:15 +0600

    Create useful section

---
+++ app/assets/js/useful.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.useful-swiper', {
+        loop: true,
+        slidesPerView: 1,
+        slideActiveClass: 'useful-slide--active',
+        slideNextClass: 'useful-slide--next',
+        slidePrevClass: 'useful-slide--prev',
+        centeredSlides: true,
+
+        speed: 500,
+
+        breakpoints: {
+            1024: {
+                slidesPerView: 3
+            }
+        },
+      
+        // If we need pagination
+        pagination: {
+          el: '.useful-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.useful-btn-next',
+          prevEl: '.useful-btn-prev',
+        },
+    });
+})
